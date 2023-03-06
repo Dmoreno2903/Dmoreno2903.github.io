1c3b377712cb977aab3e2884256ff4f8a02e87fb
Angel Brand <angeldavidbran000@gmail.com>
2023-03-06 12:35:15 -0500

    mar612

--- wordle.js
+++ wordle.js
@@ -1,5 +1,6 @@
 
-var word = "angelb"
+var ver = {};
+var word = "angel"
 var ab = ['a','b','c','d','e','f','g','h','i','j','k','l', 'ñ','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 window.onload = ()=>{
     initialize();
@@ -11,7 +12,7 @@ let initialize = () =>{
         col.style = "display:flex";
         for (let j = 0; j < word.length; j++) {
             let tile = document.createElement("input");
-            let id = ((6*i)+j);
+            let id = ((word.length*i)+j);
             tile.id = id+"tile";
             tile.classList.add("tile");
             tile.type = "text";
@@ -19,11 +20,17 @@ let initialize = () =>{
                 if(ab.includes(e.key.toLocaleLowerCase())){
                     if(e.target.value)e.preventDefault();
                     else if(id<((6*word.length)-1)) setTimeout(()=>{
+                        ver={...ver,[id]:e.key}
                         document.getElementById((id+1)+"tile").focus();
+                        console.log(id)
+                        if(id>0 && id===(i*word.length)+(word.length-1)) {
+                            console.log("ver",id)
+                            verify(i)
+                        }
                     },1)
                 }
                 else if(id>=1 && e.key==="Backspace"){
-                    document.getElementById((id-1)+"tile").focus();
+                    if(id!==i*word.length)document.getElementById((id-1)+"tile").focus();
                 }
                 else{
                     e.preventDefault();
@@ -37,4 +44,21 @@ let initialize = () =>{
     }
 
     document.getElementById("0tile").focus();
+}
+
+let verify = (i)=>{
+    let correct = 0
+    let indP = 0
+    for (let ind = i*word.length; ind <= (i*word.length)+(word.length-1); ind++) {
+        let el = document.getElementById(ind+"tile")
+        if(ver[ind]===word[indP]){
+            el.classList.add("correct")
+        }else if(word.includes(ver[ind])){
+            el.classList.add("present")
+        }else{
+            el.classList.add("absent")
+        }
+        
+        indP++
+    }
 }
