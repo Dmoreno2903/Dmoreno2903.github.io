527f42e9bfadaa3bcfd3ce1cf84464681d5710ea
Angel Brand <AngelBrand1>
2023-03-09 20:28:31 -0500

    mar9

--- wordle.js
+++ wordle.js
@@ -3,18 +3,19 @@ var word = "";
 var data_base = null;
 var ab = ['a','b','c','d','e','f','g','h','i','j','k','l', 'ñ','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_'];
 window.onload = ()=>{
-    fetch('/base_datos.json').then(res=>res.json()).then(initialize)
+    fetch('/base_datos.json').then(res=>res.json()).then(initialize);
 }
 
 let initialize = (db) =>{
     data_base = db;
     i = Math.floor(Math.random()*Object.keys(db).length);
     let obj = Object.entries(db);
-    word=obj[i][0]
+    word=obj[i][0];
+    console.log(word);
     for (let i = 0; i < 3; i++) {
         let col = document.createElement("div");
         col.style = "display:flex";
-        for (let j = 0; j < word.length; j++) {
+        for (let j = 0; j < word.length; j++) { 
             let tile = document.createElement("input");
             let id = ((word.length*i)+j);
             tile.id = id+"tile";
@@ -23,15 +24,13 @@ let initialize = (db) =>{
             tile.addEventListener("keydown",(e)=>{
                 if(ab.includes(e.key.toLocaleLowerCase())){
                     if(e.target.value)e.preventDefault();
-                    else if(id<((6*word.length)-1)) setTimeout(()=>{
+                    else if(id<((3*word.length))) setTimeout(()=>{
                         ver={...ver,[id]:e.key.toUpperCase()}
-                        console.log(ver)
-                        document.getElementById((id+1)+"tile").focus();
-                        if(id>0 && id===(i*word.length)+(word.length-1)) {
-                            console.log(id)
-                            verify(i)
-                        }
+                        console.log(id)
+                        if(id>0 && id===(i*word.length)+(word.length-1))verify(i)
+                        document.getElementById((id+1)+"tile")?.focus();
                     },1)
+
                 }
                 else if(id>=1 && e.key==="Backspace"){
                     if(id!==i*word.length && id!=word.length*6-1)document.getElementById((id-1)+"tile").focus();
@@ -51,12 +50,17 @@ let initialize = (db) =>{
 }
 
 let verify = (i)=>{
+    correct = 0
     let aux = word
     let indP = 0
     for (let ind = i*word.length; ind <= (i*word.length)+(word.length-1); ind++) {
         let el = document.getElementById(ind+"tile")
-        console.log(ver,word)
+        // console.log(ver,word)
         if(ver[ind]===word[indP]){
+            correct++
+            if(correct===word.length){
+                window.confirm("¡correcta!");
+            }
             aux = aux.slice(0,indP)+" "+aux.slice(indP+1)
             el.classList.add("correct")
             
@@ -66,6 +70,7 @@ let verify = (i)=>{
         }else{
             el.classList.add("absent")
         }
+        
         indP++
     }
 }
